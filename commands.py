46d7e105418151a5468c24a29777a538fa88aa0f
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-29 15:25:11 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -9,10 +9,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
+    await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS,
-        reply_to_message_id=update.message_id
+        reply_markup=Translation.START_BUTTONS
     )
